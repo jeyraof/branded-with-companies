06a18d10b34e9cd7d4c91805552ec2072fe81ac9
Jaeyoung Lee <jaeyoung@monodiary.net>
2019-12-09 06:58:18 +0900

    deploy

---
+++ build/precache-manifest.80ee76fc74a0d0222d85fd4ad31ccf10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdeaeb4724bde04ec297fb9e222a80d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4afc0af4f50d455cf907",
+    "url": "/static/css/main.1b88f56f.chunk.css"
+  },
+  {
+    "revision": "9e4ce8786733df4f21c3",
+    "url": "/static/js/2.6714309b.chunk.js"
+  },
+  {
+    "revision": "4afc0af4f50d455cf907",
+    "url": "/static/js/main.24976b32.chunk.js"
+  },
+  {
+    "revision": "e84aa2a739c6039ea75d",
+    "url": "/static/js/runtime-main.b3419bac.js"
+  }
+]);
